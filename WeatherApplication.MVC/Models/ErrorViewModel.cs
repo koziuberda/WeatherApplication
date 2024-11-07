f1ea110e04db55b9668ef70cf869f5711f87d474
Severyn Koziuberda <koziuberdaseverin@gmail.com>
2024-11-07 16:04:19 +0200

    Initial commit

---
+++ WeatherApplication.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherApplication.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
